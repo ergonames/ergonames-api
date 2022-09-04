cbf0bb226a1d60db7744c08fbccf7ea20aa203c9
JC Gonzalez <programmer1718@gmail.com>
2022-09-04 16:01:46 -0400

    update model for queue messages

--- src/ErgoNames.Api/Models/ProcessMintingRequest.cs
+++ src/ErgoNames.Api/Models/ProcessMintingRequest.cs
@@ -1,14 +1,14 @@
 namespace ErgoNames.Api.Models;
 
-public class EnqueueMintingRequest
+public class ProcessMintingRequest
 {
     /// <summary>
     /// The ID of the transaction that submitted payment to the minting contract
     /// </summary>
-    public int PaymentTxId { get; set; }
+    public string PaymentTxId { get; set; }
     
     /// <summary>
     /// The ID of the minting request box
     /// </summary>
-    public int MintingRequestBoxId { get; set; }
+    public string MintingRequestBoxId { get; set; }
 }
